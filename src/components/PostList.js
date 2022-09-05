9160b70cfbce0217cb90c7007114ba5cc330b348
Ayush Indoria <96787650+akkii03@users.noreply.github.com>
2022-09-06 00:17:11 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,32 +3,30 @@ import { fetchPosts } from '../api/fetchPosts'
 import {Post} from './Post';
 import { PaginationButtonsList } from './PaginationButtonsList';
 
+
 const PostList = () => {
-    const [page,setPage]=useState(1);
-    const [posts,setPosts]=useState([]);
-    const [isLoading,setIsLoading] =useState(true);
+    const[page,setPage] = useState(1);
+    const[posts,setPosts] = useState([]);
+    const[isLoading,setIsLoading] = useState(true);
     const limit = 5;
-    useEffect(()=>{
+    useEffect(()=> {
         setIsLoading(true);
-        fetchPosts(page,limit).then((res)=>{
+        fetchPosts(page,limit).then((response)=>{
             setIsLoading(false);
-            setPosts([...res.data]); 
-        });
+            setPosts([...response.data])
+        })
     },[page]);
-
-    function handleChange(e)
-    {
-        setPage(e.target.value);
+    
+    function handleChange(event) {
+        setPage(event.target.value);
     }
-
-    console.log('posts is',posts);
     return (
         <>
-        {isLoading &&<h1>Loading .........</h1>}
-        {!isLoading && posts.map((item,i)=>{
-            return <Post title={item.title} body={item.body} id={i}></Post>
+        {isLoading && <h1 className="loader">Loading</h1>}
+         {!isLoading && posts.map((item,i)=>{
+            return <Post title={item.title} body={item.body} id={i}/>
         })}
-        <PaginationButtonsList handleChange={handleChange}></PaginationButtonsList>
+            <PaginationButtonsList handleChange={handleChange}/>
         </>
     )
 }
